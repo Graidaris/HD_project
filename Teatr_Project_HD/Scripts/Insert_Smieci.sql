441aff148a44ec7d6d6ef6cd3ab0e48c3770909e
Władysław Jakołcewicz <vladislavby96@gmail.com>
2018-12-15 00:03:34 +0100

    Added Inne, Smieci, Czas.

---
+++ Teatr_Project_HD/Scripts/Insert_Smieci.sql
@@ -0,0 +1,6 @@
+INSERT INTO Smieci VALUES ('informacja', 'gotówka');
+INSERT INTO Smieci VALUES ('drzwiGlowne', 'karta');
+INSERT INTO Smieci VALUES ('drzwiBoczne', 'gotowka');
+INSERT INTO Smieci VALUES ('informacja', 'karta');
+INSERT INTO Smieci VALUES ('drzwiGlowne', 'gotowka');
+INSERT INTO Smieci VALUES ('drzwiBoczne', 'karta');
